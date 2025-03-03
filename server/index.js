0cc19fd650cc5997caa5e8e6c095640ace160853
Hema <149656888+Hepsibaalkolu@users.noreply.github.com>
2025-03-03 15:16:49 +0530

    ok

--- server/index.js
+++ server/index.js
@@ -13,7 +13,9 @@ app.use(cors(
         origin: ['https://website-app-client.vercel.app'],
         
         methods: true,
-        credentials: true
+        credentials: true,
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+        allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept']
     }
 ));
 app.use(express.json());
