45f40946db263822e8e6f925e388ddce45e281cd
Hema <149656888+Hepsibaalkolu@users.noreply.github.com>
2025-03-04 10:51:52 +0530

    update

--- server/index.js
+++ server/index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const mongoose = require('mongoose');
 const cors = require("cors");
 const UserModel = require('./models/Users');
-const dotenv = require('dotenv');
+// const dotenv = require('dotenv');
 
-dotenv.config();
+// dotenv.config();
+require('dotenv').config();
+
 const app = express();
 
 app.use(cors());
